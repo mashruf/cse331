991a397fa08a743ff26f1bc83e0065232a0ed841
Mashruf Mahabub <mashruf.mahboob@northsouth.edu>
2020-12-06 16:31:09 +0600

    1stAssignment_2ndTask

---
+++ Task2.asm
@@ -0,0 +1,27 @@
+.MODEL SMALL
+.STACK 100H
+.CODE
+    
+    MOV AH, 1
+    INT 21H
+    MOV BL, AL
+    
+    MOV AH, 1
+    INT 21H
+    MOV Bh, AL
+    
+    MOV AH, 2
+    MOV DL, 20H
+    INT 21H
+    
+    MOV AH, 2
+    MOV DL, BL
+    INT 21H
+    
+    MOV AH, 2
+    MOV DL, BH
+    INT 21H
+    
+    
+    MOV AH, 4CH
+    INT 21H
