cbd968dc7f4545f60af9fb495cb97f8291401cf2
Mashruf Mahabub <mashruf.mahboob@northsouth.edu>
2020-12-06 16:34:56 +0600

    2ndAssignment_2ndTask

--- Task2.asm
+++ Task2.asm
@@ -1,27 +1,50 @@
 .MODEL SMALL
 .STACK 100H
+.DATA
+
+    S DB "rohon$"
+    
 .CODE
+
+    MOV AX, @DATA
+    MOV DS, AX 
     
-    MOV AH, 1
-    INT 21H
-    MOV BL, AL
     
-    MOV AH, 1
-    INT 21H
-    MOV Bh, AL
-    
+    LEA SI, S
     MOV AH, 2
-    MOV DL, 20H
-    INT 21H
     
-    MOV AH, 2
-    MOV DL, BL
-    INT 21H
-    
-    MOV AH, 2
-    MOV DL, BH
+    MOV AL, [SI]
+    SUB AL, 20H
+    MOV DL, AL
     INT 21H
     
     
-    MOV AH, 4CH
+    
+    INC SI
+    MOV AL, [SI]
+    SUB AL, 20H
+    MOV DL, AL
+    INT 21H 
+    
+    INC SI
+    MOV AL, [SI]
+    SUB AL, 20H
+    MOV DL, AL
     INT 21H
+    
+    INC SI
+    MOV AL, [SI]
+    SUB AL, 20H
+    MOV DL, AL
+    INT 21H
+    
+    INC SI
+    MOV AL, [SI]
+    SUB AL, 20H
+    MOV DL, AL
+    INT 21H
+    
+      
+MOV AH, 4CH
+INT 21H
+    
